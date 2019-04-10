80c0d5ddbae8e31a7c019302bff22779e3486863
JustinRobertKing <kingj4@spu.edu>
2019-04-09 17:20:38 -0700

    done

--- controllers/cryptids.js
+++ controllers/cryptids.js
@@ -3,35 +3,35 @@ var router = express.Router()
 
 var fs = require('fs')
 
-var dinoData = fs.readFileSync('./dinosaurs.json')
-dinoData = JSON.parse(dinoData)
+var cryptidData = fs.readFileSync('./cryptids.json')
+cryptidData = JSON.parse(cryptidData)
 
 // index / GET
 router.get('/', (req, res) => {
-	res.render('dinosaurs/index', { myDinos: dinoData })
+	res.render('cryptids/index', { myCryptids: cryptidData })
 })
 
 // new /new GET
 router.get('/new', (req, res) => {
-	res.render('dinosaurs/new')
+	res.render('cryptids/new')
 })
 
 // create / POST
 router.post('/', (req, res) => {
-	dinoData.push(req.body)
-	fs.writeFileSync('.dinosaurs.json', JSON.stringify(dinoData))
-	res.redirect('/dinosaurs')
+	cryptidData.push(req.body)
+	fs.writeFileSync('.cryptids.json', JSON.stringify(cryptidData))
+	res.redirect('/cryptids')
 })
 
 // show /:id GET
 router.get('/:id', (req, res) => {
-	var dinoIndex = parseInt(req.params.id)
-	res.render('dinosaurs/show', { myDino: dinoData[dinoIndex] })
+	var cryptidIndex = parseInt(req.params.id)
+	res.render('cryptids/show', { myCryptid: cryptidData[cryptidIndex] })
 })
 
 // edit /edit/:id GET
 router.get('/edit/:id', (req, res) => {
-	res.render('dinosaurs/edit')
+	res.render('cryptids/edit')
 })
 
 // update /:id PUT
